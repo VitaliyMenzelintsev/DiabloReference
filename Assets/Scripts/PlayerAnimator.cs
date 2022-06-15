8ae1452f93e0dfb4710bce60062b5dd4d43b598c
VitaliyMenzelintsev <erick.lenshr@gmail.com>
2022-06-16 01:38:04 +0400

    Animation Update

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(PlayerController))]
+using UnityEngine.AI;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    public AnimationClip RunAnimation;
-    public AnimationClip IdleAnimation;
+    const float LocomotionAnimationSmoothTime = 0.1f;
+
+    Animator CharacterAnimator;
+    NavMeshAgent NavMeshAgent;
+
+    private void Start()
+    {
+        CharacterAnimator = GetComponent<Animator>(); // если аниматор висит не на родительском обьекте, а на дочерней графике нужно использовать GetComponentInChildren
+        NavMeshAgent = GetComponent<NavMeshAgent>(); 
+    }
+
+    private void Update()
+    {
+        float _speedPercent = NavMeshAgent.velocity.magnitude / CharacterAnimator.speed;
+        CharacterAnimator.SetFloat("speedPercent", _speedPercent, LocomotionAnimationSmoothTime, Time.deltaTime);
+    }
 
     public void Running()
     {
-        GetComponent<Animation>().CrossFade(RunAnimation.name);
+
     }
 
     public void Idle()
     {
-        GetComponent<Animation>().CrossFade(IdleAnimation.name);
+
     }
 }
